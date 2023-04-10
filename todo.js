a6d5d0011b6f2cab8a65cbd67feadfb818272179
TildeMarkstrom <tilde_markstrom@hotmail.com>
2023-04-10 22:34:33 +0200

    storage progress

--- todo.js
+++ todo.js
@@ -2,13 +2,15 @@ const addTaskButton = document.getElementById("addButton");
 const inputTask = document.getElementById("taskInput");
 const taskListMemory = [];
 
+let taskDone = false;
+
 //function get storage
 //getTaskStorage();
 
 //function listen for click on "add task" button
 addTaskButton.addEventListener('click', () =>{
     if(inputTask.value.length > 0){
-        //setTaskStorage();
+        setTaskStorage();
         createNewTask();
         clearInputText();
 }});
@@ -29,6 +31,13 @@ function createNewTask(){
     // the following 6 lines were conducted from https://www.youtube.com/watch?v=vnNQaKXXJiU
     taskParagraph.classList.add('taskText');
     taskDiv.classList.add('task');
+    taskDiv.id = new Date().getTime();
+    // 5 lines samira
+    /* if (id === ""){
+    taskDiv.id = new Date().getTime();
+    } else{
+    taskDiv.id = id;
+    }  */
     taskButton.classList.add('buttons');
     taskCheckmark.classList.add('checkmark');
     taskCross.classList.add('cross');
@@ -45,23 +54,48 @@ function createNewTask(){
     //function checkmark -> line over text
     taskCheckmark.addEventListener('click', () =>{
         taskParagraph.style.textDecoration = "line-through";
+        let taskDone = true;
     });
 
     //function remove -> remove
     taskCross.addEventListener('click', () =>{
         taskList.removeChild(taskDiv);
-        taskDiv.removeChild(taskParagraph);
+        removeFromStorage(taskDivId);
+
+        /* taskDiv.removeChild(taskParagraph);
         taskDiv.removeChild(taskButton);
         taskButton.removeChild(taskCheckmark);
         taskButton.removeChild(taskCross);
+        can be removed */
     });
+
+    //function creating object
+    //the following 4 lines were adapted by the help of Samira Leonhardt
+    let taskDivId = taskDiv.id;
+    let task = {
+        taskName : inputTask.value,
+        checkmark : taskDone,
+        id : taskDivId,
+    }
+
+    let taskString = JSON.stringify(task);
+
+    setTaskStorage(taskDivId, taskString);
+
+    console.log(task);
 }
 
 //function storage
-/* function setTaskStorage(){
-    localStorage.setItem('task', taskInput.value);
+ function setTaskStorage(taskDivId, taskString){
+    localStorage.setItem(taskDivId, taskString);
+    //console.log(taskString);
 }
 
-function getTaskStorage(){
+/* function getTaskStorage(taskString){
     localStorage.getItem('task');
-} */
+}  */
+
+//function remove object from storage
+function removeFromStorage(taskDivId){
+    localStorage.removeItem(taskDivId);
+}
